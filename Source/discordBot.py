fddbba76ee54a335122c3a88c092200c06482777
Jack-Rouleau <jack.rouleau.jr@gmail.com>
2017-09-12 19:13:39 -0400

    Initial commit

---
+++ Source/discordBot.py
@@ -0,0 +1,34 @@
+import discord
+import asyncio
+import random
+import os
+import json
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-----')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!addquote'):
+        if not os.path.isfile("quote_file.json"):
+            quote_list = []
+        else:
+            with open("quote_file.json", "r") as quote_file:
+                quote_list = json.load(quote_file)
+        quote_list.append(message.content[10:])
+        with open("quote_file.json", "w") as quote_file:
+            json.dump(quote_list, quote_file)
+    elif message.content.startswith('!quote'):
+        with open("quote_file.json", "r") as quote_file:
+            quote_list = json.load(quote_file)
+        await client.send_message(message.channel, random.choice(quote_list))
+
+
+client.run('MzU1ODAyMzAwMzUyMjk5MDEw.DJSGQw.Lc1sFWWyguwjknssbrybqw5pE2A')
+
